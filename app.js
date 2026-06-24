6def87e3da726e314afd9e517dab2d3b8c754b30
kiesh-tech06 <charlene.alyssa.nathan1@student.mmu.edu.my>
2026-06-24 23:12:36 +0800

    completed source code template

---
+++ app.js
@@ -0,0 +1,20 @@
+//navbar func
+$(document).ready(function(){
+
+    $('.fa-bars').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('scroll load', function(){
+        $('fa-bars').toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+
+        if($(window).scrollTop() > 30){
+        $('header').addClass('header-active');
+        }else{
+        $('header').removeClass('header-active');
+        }
+    });
+
+});
